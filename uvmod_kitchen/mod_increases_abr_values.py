98e3fc50d4495b8015a6490da541ff25d31ac31e
IK8JHL <francescocasertano@libero.it>
2023-08-18 18:48:39 +0200

    Update mod_increases_abr_values.py

--- uvmod_kitchen/mod_increases_abr_values.py
+++ uvmod_kitchen/mod_increases_abr_values.py
@@ -1,10 +1,10 @@
 # This mod allows you to increase the value of the ABR by 2 by 4 or by 8 , remove # at the beginning of the line , according to the value of interest
 
 
-# multi=int(64) # 
-multi=int(128) # (1 corresponds to 2 seconds, 2 corresponds to 4 seconds... up to 5, which corresponds to 10 seconds)
-# multi=int(192) # (1 corresponds to 4 seconds, 2 corresponds to 8 seconds... up to 5, which corresponds to 20 seconds)
-# multi=int(256) # (1 corresponds to 8 seconds, 2 corresponds to 16 seconds... up to 5, which corresponds to 40 seconds)
+# multi = int(64) # 
+multi = int(128) # (1 corresponds to 2 seconds, 2 corresponds to 4 seconds... up to 5, which corresponds to 10 seconds)
+# multi = int(192) # (1 corresponds to 4 seconds, 2 corresponds to 8 seconds... up to 5, which corresponds to 20 seconds)
+# multi = int(256) # (1 corresponds to 8 seconds, 2 corresponds to 16 seconds... up to 5, which corresponds to 40 seconds)
 ##--------------------- do not modify below this line ---------------------------------------------------
 import os,sys,struct
 print('Running',os.path.basename(sys.argv[0]),'mod...')
